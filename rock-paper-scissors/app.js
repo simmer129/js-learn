a77a3eb982e6b63eb2718fa00067b06bbceb07fd
simmer129 <simmer641@gmail.com>
2023-11-19 22:29:01 +0330

    refactor result

--- rock-paper-scissors/app.js
+++ rock-paper-scissors/app.js
@@ -2,20 +2,30 @@ const choices = ["rock", "paper", "scissors"];
 
 const checkWinner = (player, computer) => {
   if (player === computer) {
-    return "It's a tie.";
+    return "dorw";
   } else if (player === "rock") {
-    return computer === "scissors" ? "Player win" : "computer win";
+    return computer === "scissors" ? "player" : "computer";
   } else if (player === "paper") {
-    return computer === "rock" ? "Player win" : "computer win";
+    return computer === "rock" ? "player" : "computer";
   } else {
-    return computer === "paper" ? "Player win" : "computer win";
+    return computer === "paper" ? "player" : "computer";
+  }
+};
+
+const showResult = (result) => {
+  if (result === "player") {
+    console.log("You win.");
+  } else if (result === "computer") {
+    console.log("You lose!");
+  } else {
+    console.log("It's a tie.");
   }
 };
 
 const play = () => {
   const playerChoice = prompt("Choose rock, paper, scissors");
 
-  if (choices.indexOf(playerChoice) !== -1) {
+  if (choices.indexOf(playerChoice?.toLowerCase()) !== -1) {
     console.log(`You chooes: ${playerChoice}`);
   } else {
     console.log("You cheated!");
@@ -26,8 +36,7 @@ const play = () => {
   const computerChoice = choices[randomNumber];
   console.log(`Computer chooses: ${computerChoice}`);
 
-  const result = checkWinner(playerChoice, computerChoice);
-  console.log(result);
+  const res = checkWinner(playerChoice, computerChoice);
+  showResult(res);
 };
 play();
-
